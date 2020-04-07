c9b758d9d12a18f25bf56d2b31f0a8fee4ac059e
oseias <oseiasal@gmail.com>
2020-04-07 11:33:46 -0300

    adicionado react router

--- src/App.js
+++ src/App.js
@@ -1,38 +1,62 @@
-import React from 'react';
+import React, { useEffect } from 'react';
+import { Jumbotron } from 'react-bootstrap';
+import { BrowserRouter as Router, Link, Route, Switch } from "react-router-dom";
 import './App.css';
 import Header from './components/Header';
 import Orders from './components/Orders';
 import Register from './components/Register';
 import Warehouse from './components/Warehouse';
+import Login from './components/Login'
 
-import {
-  BrowserRouter as Router,
-  Switch,
-  Route,
-  Link
-} from "react-router-dom";
+import { getMenu } from './API/PastelAPI'
+
 
 function App() {
+
+  useEffect(() => {
+    getMenu();
+  })
+
   return (
     <Router>
-      <Header  register={<Link to="/register">Register</Link>} 
-      warehouse={<Link to="/warehouse">Warehouse</Link>}
-      orders={<Link to="/orders">Orders</Link>} 
+      <Header
+        login={<Login />}
+        home={<Link to="/">Home</Link>}
+        register={<Link to="/register">Register</Link>}
+        warehouse={<Link to="/warehouse">Warehouse</Link>}
+        orders={<Link to="/orders">Orders</Link>}
+
       />
       <Switch>
-          <Route exact path="/register">
-          <Register />
-          </Route>
 
-          <Route exact path="/warehouse">
+        <Route exact path="/">
+          <Jumbotron style={{ height: '90vh' }}>
+
+            <h1 class="display-3">Jumbo heading</h1>
+            <p class="lead">Jumbo helper text</p>
+            <p>More info</p>
+            <p class="lead">
+              <a class="btn btn-primary btn-lg" href="Jumbo action link" role="button">Jumbo action name</a>
+            </p>
+
+          </Jumbotron>
+
+
+        </Route>
+
+        <Route exact path="/register">
+          <Register />
+        </Route>
+
+        <Route exact path="/warehouse">
           <Warehouse />
-          </Route>
-      
-      
-          <Route exact path="/orders">
+        </Route>
+
+
+        <Route exact path="/orders">
           <Orders />
-          </Route>
-     
+        </Route>
+
       </Switch>
     </Router>
   );
